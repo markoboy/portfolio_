51e817ca83c63f2a977dcd5e1550ba2abe8698d0
Athanasios Markou <thanasismarko@gmail.com>
2018-10-03 15:27:52 +0100

    feat: Add header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,21 @@ class Header extends Component {
 	render() {
 		return (
 			<header className="header">
-				<h1>Athanasios Markou</h1>
-				<nav className="navlink_container">
-					<ul className="navlink_list">
-						<li className="navlink_item"><NavLink exact to="/">Home</NavLink></li>
-						<li className="navlink_item"><NavLink to="/portfolio">Portfolio</NavLink></li>
-						<li className="navlink_item"><NavLink to="/profile">Resume</NavLink></li>
-						<li className="navlink_item"><NavLink to="/contact">Contact</NavLink></li>
-						<li className="navlink_item"><NavLink to="/about">About</NavLink></li>
-					</ul>
-				</nav>
+				<div className="header_container">
+					<div className="header_heading">
+						<h1>Athanasios Markou</h1>
+						<p>Front-End Developer</p>
+					</div>
+					<nav className="navlink_container">
+						<ul className="navlink_list">
+							<li className="navlink_item"><NavLink exact to="/">Home</NavLink></li>
+							<li className="navlink_item"><NavLink to="/portfolio">Portfolio</NavLink></li>
+							<li className="navlink_item"><NavLink to="/profile">Resume</NavLink></li>
+							<li className="navlink_item"><NavLink to="/contact">Contact</NavLink></li>
+							<li className="navlink_item"><NavLink to="/about">About</NavLink></li>
+						</ul>
+					</nav>
+				</div>
 			</header>
 		);
 	}
