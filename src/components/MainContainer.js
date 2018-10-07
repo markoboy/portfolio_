72a4aa2ec263b29fc8f094fa5f0a6abaf8456c2a
Athanasios Markou <thanasismarko@gmail.com>
2018-10-07 16:35:56 +0100

    feat: Add homepage top container and content

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -9,16 +9,14 @@ class MainContainer extends Component {
 	render() {
 		return (
 			<div className="main">
-				<main className="main_container">
-					<Switch>
-						<Route exact path="/" component={HomePage} />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
 
-						<Route exact path="/test" component={Test} />
-						<Route exact path="/test/:id" component={Test} />
+					<Route exact path="/test" component={Test} />
+					<Route exact path="/test/:id" component={Test} />
 
-						<Route component={NotFound} />
-					</Switch>
-				</main>
+					<Route component={NotFound} />
+				</Switch>
 			</div>
 		);
 	}
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -8,12 +8,33 @@ class HomePage extends Component {
 
 	render() {
 		return (
-			<section>
-				<p>This is going to be the home page</p>
-				<p><Link to="/test">Test link</Link></p>
-				<p><Link to="/test/some_id">Test link to a test child</Link></p>
-				<p><Link to="/randompage">Random undefined route check</Link></p>
-			</section>
+			<main className="main_container">
+				<section className="home_top">
+					<div className="home_top_container">
+						<div className="home_top_content" style={{ 'alignSelf': 'flex-start'}}>
+							<p>Welcome to my personal webpage. My name is Athanasios and I am currently living in the city of Newcastle.</p>
+						</div>
+						<div className="home_top_content">
+							<p>At the moment, I am working as a Chef Assistant, but I am very passionate with coding and web development.</p>
+						</div>
+						<div className="home_top_content" style={{ 'alignSelf': 'flex-end'}}>
+							<p>Furthermore, I have recently received a certifacte from Udacity as a Front-End Web Developer. Feel free to explore this site and ask me anything from the contact form.</p>
+						</div>
+						<div className="home_top_bullets">
+							<div className="home_top_bullet"></div>
+							<div className="home_top_bullet"></div>
+							<div className="home_top_bullet"></div>
+							<div className="home_top_bullet"></div>
+						</div>
+					</div>
+				</section>
+				<section>
+					<p>This is going to be the home page</p>
+					<p><Link to="/test">Test link</Link></p>
+					<p><Link to="/test/some_id">Test link to a test child</Link></p>
+					<p><Link to="/randompage">Random undefined route check</Link></p>
+				</section>
+			</main>
 		);
 	}
 }
