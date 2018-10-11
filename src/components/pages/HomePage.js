ec67f7ff84f398e49389dbc86d04fa5bfa936b38
Athanasios Markou <thanasismarko@gmail.com>
2018-10-11 15:20:26 +0100

    feat: Add home_bottom page styles

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class HomePage extends Component {
+	constructor() {
+		super();
+		this.state = {
+			homeBottomHeight: ''
+		};
+	}
+
 	componentDidMount() {
 		document.title = 'Home Page | Athanasios Markou';
+		this.setHomeBottomHeight();
 	}
+
+	setHomeBottomHeight = () => {
+		/* Get the footer height in order for the bottom container to get full height */
+		let footer = document.querySelector('.footer');
+
+		if (footer) {
+			/* Remove 80px of the header, 50vh of the homeTopContainer and the footer height - 1 to round up the number */
+			this.setState({ homeBottomHeight: { 'minHeight': `calc(100vh - 80px - 50vh - ${footer.offsetHeight}px - 1px)` } });
+		}
+		else {
+			window.addEventListener('load', () => {
+				this.setState({ homeBottomHeight: { 'minHeight': `calc(100vh - 80px - 50vh - ${footer.offsetHeight}px - 1px)` } });
+			});
+		}
+	};
 
 	render() {
 		return (
-			<main className="main_container">
+			<main>
 				<section className="home_top">
 					<div className="home_top_container">
 						<div className="home_top_content">
@@ -22,11 +45,13 @@ class HomePage extends Component {
 						</div>
 					</div>
 				</section>
-				<section>
-					<p>This is going to be the home page</p>
-					<p><Link to="/test">Test link</Link></p>
-					<p><Link to="/test/some_id">Test link to a test child</Link></p>
-					<p><Link to="/randompage">Random undefined route check</Link></p>
+				<section className="home_bottom" style={this.state.homeBottomHeight ? this.state.homeBottomHeight : { 'minHeight': 'auto' }}>
+					<div className="home_bottom_container">
+						<p>This is going to be the home page</p>
+						<p><Link to="/test">Test link</Link></p>
+						<p><Link to="/test/some_id">Test link to a test child</Link></p>
+						<p><Link to="/randompage">Random undefined route check</Link></p>
+					</div>
 				</section>
 			</main>
 		);
